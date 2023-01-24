3e3ed7ffa777c26e0eb9fa62b3ae1880074a7534
Anselme Dor <anselmedor@hotmail.be>
2023-01-24 17:46:50 +0100

    Auth props added

--- resources/js/Pages/About.jsx
+++ resources/js/Pages/About.jsx
@@ -1,9 +1,9 @@
 import General from "@/Layouts/GeneralLayout";
 import { Head } from "@inertiajs/react";
 
-export default function About() {
+export default function About({ auth }) {
     return (
-        <General>
+        <General auth={auth}>
             <Head title="About" />
             <div className="bg-slate-300 overflow py-4">
                 <div className="text-center text-gray-900">About</div>
--- resources/js/Pages/Contact.jsx
+++ resources/js/Pages/Contact.jsx
@@ -1,9 +1,9 @@
 import General from "@/Layouts/GeneralLayout";
 import { Head } from "@inertiajs/react";
 
-export default function Contact() {
+export default function Contact({ auth }) {
     return (
-        <General>
+        <General auth={auth}>
             <Head title="Contact" />
             <div className="bg-slate-300 overflow py-4">
                 <div className="text-center text-gray-900">Contact</div>
