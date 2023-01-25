2b9e4110d049576c6239d3582c5fc176037f895d
Anselme Dor <anselmedor@hotmail.be>
2023-01-25 15:35:36 +0100

    Styling update for : display and update single course, new chapter, single course

--- resources/js/Pages/Course.jsx
+++ resources/js/Pages/Course.jsx
@@ -1,68 +1,98 @@
+import ChapterListItem from "@/Components/ChapterListItem";
 import Dropdown from "@/Components/Dropdown";
 import General from "@/Layouts/GeneralLayout";
 import { Head } from "@inertiajs/react";
+import { useState } from "react";
+import { IoBookmark } from "react-icons/io5";
 
 export default function Course({ auth, course, chapters }) {
+    const [isLiked, setIsLiked] = useState(false);
+    const toggleLiked = () => {
+        setIsLiked(!isLiked);
+    };
     return (
         <General auth={auth}>
             <Head title={`${course.title}`} />
             {/* <p>{course.status}</p> */}
-            <div className="p-10">
-                <div className="">
+            <div className="p-5 md:p-10">
+                <div>
                     <Dropdown>
                         <Dropdown.Trigger>
                             <button
                                 type="button"
-                                className="absolute -bottom-3 right-0 rounded-md  text-3xl  hover:text-red-400 focus:outline-none transition ease-in-out duration-150"
+                                className="absolute -bottom-3 right-1 rounded-md  text-3xl  hover:text-indigo-700 focus:outline-none transition ease-in-out duration-150"
                             >
                                 ...
                             </button>
                         </Dropdown.Trigger>
+                        <Dropdown.Content>
+                            <Dropdown.Link
+                                href={`/courses/${course.formatted_title}/edit`}
+                            >
+                                Edit
+                            </Dropdown.Link>
 
-                        <div className>
-                            <Dropdown.Content>
-                                <Dropdown.Link
-                                    href={`/courses/${course.formatted_title}/edit`}
-                                >
-                                    Edit
-                                </Dropdown.Link>
-
-                                <Dropdown.Link>Delete</Dropdown.Link>
-                            </Dropdown.Content>
-                        </div>
+                            <Dropdown.Link>Delete</Dropdown.Link>
+                        </Dropdown.Content>
                     </Dropdown>
                 </div>
                 <div>
-                    <h2 className="capitalize text-2xl pb-5">{course.title}</h2>
-                    <p className="capitalize px-3 py-1 mb-5 bg-gray-500 text-gray-100 w-fit rounded-full">
-                        {course.category.split("_").join(" ")}
-                    </p>
-                    <p>{course.description}</p>
-                    <img
+                    <h2 className="capitalize text-2xl pt-5 pb-5 text-center font-bold">
+                        {course.title}
+                    </h2>
+                    {/* <img
                         src={
                             "http://127.0.0.1:5174/resources/images/" +
                             course.preview_image
                         }
                         alt={course.preview_image}
-                    ></img>
+                        className="mx-auto"
+                    ></img> */}
+                    <p className="capitalize px-3 py-1 mb-5 bg-gray-500 text-gray-100 w-fit rounded-full mx-auto">
+                        {course.category.split("_").join(" ")}
+                    </p>
+                    <p className="text-center">{course.description}</p>
+                    <div
+                        onClick={toggleLiked}
+                        className="w-9/12 max-w-xs mx-auto  flex items-center justify-center space-x-4 mt-7 py-2 cursor-pointer bg-indigo-700 text-white rounded-md shadow-lg hover:bg-indigo-800 hover:ring-2 hover:ring-offset-2 hover:ring-indigo-700  transition-all"
+                    >
+                        <IoBookmark
+                            size={20}
+                            className={`top-1 right-1 stroke-[20px] stroke-white fill-transparent ${
+                                isLiked ? "fill-yellow-400" : "fill-transparent"
+                            }`}
+                        />
+                        <p>Follow</p>
+                    </div>
                 </div>
             </div>
-            <div className="p-10">
-                <h3 className="underline">Chapters:</h3>
-                {chapters.map((chapter, key) => (
-                    <div key={key}>
-                        <a
-                            href={
-                                "/courses/" +
-                                course.formatted_title +
-                                "/" +
-                                chapter.formatted_title
-                            }
-                        >
-                            {chapter.title}
-                        </a>
-                    </div>
-                ))}
+            <div className="mb-5">
+                <h3 className="underline text-xl text-center">
+                    {chapters.length > 0 ? "" : "No chapter"}
+                </h3>
+                <div>
+                    <ol className="mx-auto w-10/12 lg:w-3/5 xl:w-1/2">
+                        {chapters.map((chapter, key) => (
+                            <li key={key}>
+                                <a
+                                    href={
+                                        "/courses/" +
+                                        course.formatted_title +
+                                        "/" +
+                                        chapter.formatted_title
+                                    }
+                                    className="cursor-default"
+                                >
+                                    <ChapterListItem
+                                        title={chapter.title}
+                                        description={chapter.description}
+                                        id={chapter.id}
+                                    />
+                                </a>
+                            </li>
+                        ))}
+                    </ol>
+                </div>
             </div>
         </General>
     );
--- resources/js/Pages/UpdateCourse.jsx
+++ resources/js/Pages/UpdateCourse.jsx
@@ -1,15 +1,16 @@
-import {router, useForm, usePage} from "@inertiajs/react";
+import { router, useForm, usePage } from "@inertiajs/react";
 
 export default function UpdateCourse() {
-
     const { course } = usePage().props;
 
-    const { data, setData, post} = useForm({
+    const { data, setData, post } = useForm({
         title: course.title,
         description: course.description,
         category: course.category,
         preview_image: course.preview_image,
         last_preview_image: course.preview_image,
+
+        _method: "patch",
 
         // NOTE: When working with Laravel PUT/PATCH requests and FormData
         // you SHOULD send POST request and fake the PUT request like this.
@@ -18,21 +19,49 @@ export default function UpdateCourse() {
     function submit(e) {
         e.preventDefault();
 
-        patch('/courses' + course.formatted_title + '/update');
-        //router.post('/courses/' + course.formatted_title + '/update', data);
+        // patch("/courses" + course.formatted_title + "/update");
+        post("/courses/" + course.formatted_title + "/update", {
+            forceFormData: true,
+        });
     }
 
     return (
         <div>
             <form onSubmit={submit} className="flex flex-col p-10">
                 <h2 className="underline">Course:</h2>
-                <input onChange={e => setData('title', e.target.value)} name="title" type="text" defaultValue={course.title} />
-                <textarea onChange={e => setData('description', e.target.value)} name="description" defaultValue={course.description} />
-                <input onChange={e => setData('category', e.target.value)} name="category" type="text" defaultValue={course.category} />
-                <input onChange={e => setData('preview_image', e.target.value)} name="preview_image" type="file" />
-                <img src={"http://127.0.0.1:5174/resources/images/" + course.preview_image} alt={course.preview_image}></img>
-                <button type="submit" className="bg-red-500 text-amber-50 p-5">CHANGE THE COURSE</button>
+                <input
+                    onChange={(e) => setData("title", e.target.value)}
+                    name="title"
+                    type="text"
+                    value={course.title}
+                />
+                <textarea
+                    onChange={(e) => setData("description", e.target.value)}
+                    name="description"
+                    defaultValue={course.description}
+                />
+                <input
+                    onChange={(e) => setData("category", e.target.value)}
+                    name="category"
+                    type="text"
+                    defaultValue={course.category}
+                />
+                <input
+                    onChange={(e) => setData("preview_image", e.target.value)}
+                    name="preview_image"
+                    type="file"
+                />
+                <img
+                    src={
+                        "http://127.0.0.1:5174/resources/images/" +
+                        course.preview_image
+                    }
+                    alt={course.preview_image}
+                ></img>
+                <button type="submit" className="bg-red-500 text-amber-50 p-5">
+                    CHANGE THE COURSE
+                </button>
             </form>
         </div>
-    )
+    );
 }
