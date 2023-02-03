af5d0158fafa1a007cf2f52addccd50c9ddfcf8e
Anselme Dor <anselmedor@hotmail.be>
2023-02-03 12:31:10 +0100

    Fixing responsive

--- resources/js/Pages/About.jsx
+++ resources/js/Pages/About.jsx
@@ -11,7 +11,6 @@ import "aos/dist/aos.css";
 /*min-h-[calc(100vh-125px)]*/
 
 export default function About({ auth }) {
-
     useEffect(() => {
         AOS.init();
     }, []);
@@ -19,25 +18,63 @@ export default function About({ auth }) {
     return (
         <General auth={auth}>
             <Head title="About" />
-            <div className="bg-gray-200">
-                <div className="flex flex-col items-start pt-16 min-h-[calc(100vh-125px)]">
-                    <div className="flex mx-auto w-full" data-aos="fade-right">
-                        <h2 className="mx-auto text-medium pt-10 text-4xl text-gray-700 font-semibold">Learn more about us...</h2>
+            <div className="bg-gradient-to-r from-gray-50 to-gray-200 overflow-hidden">
+                <div className="py-16 lg:py-28 min-h-[calc(100vh-125px)]">
+                    <div data-aos="fade-right">
+                        <h2 className="mx-auto w-[90%] text-center text-medium text-4xl text-gray-700 font-semibold">
+                            Learn more about{" "}
+                            <span className="text-indigo-800">us</span> ...
+                        </h2>
                     </div>
-                    <div data-aos="fade-left" className="flex lg:flex-row flex-col items-center justify-between w-3/4 mx-auto">
-                        <Player
-                            autoplay
-                            loop
-                            src="/assets/81450-team.json"
-                            className="w-[90%]"
-                        >
-                        </Player>
+                    <div
+                        data-aos="fade-left"
+                        className="flex lg:flex-row flex-col items-center justify-between w-3/4 mx-auto"
+                    >
+                        <div className="max-w-lg h-auto 2xl:max-w-3xl 2xl:pr-44">
+                            <Player
+                                autoplay
+                                loop
+                                src="/assets/81450-team.json"
+                            ></Player>
+                        </div>
                         <div data-aos="fade-left">
                             <p className="text-medium text-lg mt-5 mb-5">
-                                At <span className="text-indigo-800">School Helper</span>, we believe that every student has the potential to be a hero in their <span className="text-indigo-800">own learning journey</span>. Our platform is <span className="text-indigo-800">designed</span> to <span className="text-indigo-800">empower</span> and <span className="text-indigo-800">inspire</span> students to reach their <span className="text-indigo-800">full potential</span> through engaging, interactive and personalized learning experiences. With a focus on <span className="text-indigo-800">making education accessible to all</span>, we're on a mission to change the way the world learns and create a <span className="text-indigo-800">new generation of heroes</span>, one student at a <span className="text-indigo-800">time</span>.
+                                At{" "}
+                                <span className="text-indigo-800">
+                                    School Helper
+                                </span>
+                                , we believe that every student has the
+                                potential to be a hero in their{" "}
+                                <span className="text-indigo-800">
+                                    own learning journey
+                                </span>
+                                . Our platform is{" "}
+                                <span className="text-indigo-800">
+                                    designed
+                                </span>{" "}
+                                to{" "}
+                                <span className="text-indigo-800">empower</span>{" "}
+                                and{" "}
+                                <span className="text-indigo-800">inspire</span>{" "}
+                                students to reach their{" "}
+                                <span className="text-indigo-800">
+                                    full potential
+                                </span>{" "}
+                                through engaging, interactive and personalized
+                                learning experiences. With a focus on{" "}
+                                <span className="text-indigo-800">
+                                    making education accessible to all
+                                </span>
+                                , we're on a mission to change the way the world
+                                learns and create a{" "}
+                                <span className="text-indigo-800">
+                                    new generation of heroes
+                                </span>
+                                , one student at a{" "}
+                                <span className="text-indigo-800">time</span>.
                             </p>
-                            <div className="flex justify-start">
-                                <PrimaryButton  children={"CONTACT US"}>
+                            <div className="flex justify-center mt-20">
+                                <PrimaryButton children={"CONTACT US"}>
                                     <IoMail size={20} className="mr-7" />
                                     <p className="text-sm">Contact Us</p>
                                 </PrimaryButton>
@@ -46,17 +83,28 @@ export default function About({ auth }) {
                     </div>
                 </div>
 
-                <hr className="mx-auto h-px my-8 bg-gray-300 border-0 dark:bg-gray-700 w-3/4 mb-16" data-aos="fade-left" />
+                <hr
+                    className="mx-auto h-px my-8 bg-gray-300 border-0 dark:bg-gray-700 w-3/4 mb-16"
+                    data-aos="fade-left"
+                />
 
                 <div className="flex mx-auto w-full" data-aos="fade-bottom">
-                    <h2 className="mx-auto text-medium pt-10 text-4xl text-gray-700 font-semibold">Our Team</h2>
+                    <h2 className="mx-auto text-medium pt-10 text-4xl text-gray-700 font-semibold">
+                        Our Team
+                    </h2>
                 </div>
 
-                <div data-aos="fade-left" className="grid grid-cols-1 md:grid-cols-2 gap-20 w-3/4 mx-auto pt-10 pb-40 bg-slate-200">
-
-                    <div className="flex flex-col justify-between items-center bg-slate-100 rounded mx-auto">
+                <div
+                    data-aos="fade-left"
+                    className="grid grid-cols-1 md:grid-cols-2 gap-20 w-3/4 mx-auto pt-10 pb-40 bg-transparent"
+                >
+                    <div className="flex flex-col justify-between items-center bg-gray-50 rounded mx-auto shadow-lg">
                         <div className="flex items-center justify-center pt-16">
-                            <img className="w-1/2 rounded-full" src="https://www.w3schools.com/howto/img_avatar.png" alt="avatar" />
+                            <img
+                                className="w-1/2 rounded-full"
+                                src="https://www.w3schools.com/howto/img_avatar.png"
+                                alt="avatar"
+                            />
                         </div>
                         <div className="flex items-center justify-center py-5 w-full">
                             <h3 className="text-2xl font-semibold">Sacha</h3>
@@ -68,13 +116,19 @@ export default function About({ auth }) {
                         </div>
                     </div>
 
-                    <div className="flex flex-col justify-between items-center bg-slate-100 rounded mx-auto">
+                    <div className="flex flex-col justify-between items-center bg-gray-50 rounded mx-auto shadow-lg">
                         <div className="flex flex-col items-center justify-center">
                             <div className="flex items-center justify-center pt-16">
-                                <img className="w-1/2 rounded-full" src="https://www.w3schools.com/w3images/avatar2.png" alt="avatar" />
+                                <img
+                                    className="w-1/2 rounded-full"
+                                    src="https://www.w3schools.com/w3images/avatar2.png"
+                                    alt="avatar"
+                                />
                             </div>
                             <div className="flex items-center justify-center py-5 w-full">
-                                <h3 className="text-2xl font-semibold">Anselme</h3>
+                                <h3 className="text-2xl font-semibold">
+                                    Anselme
+                                </h3>
                             </div>
                             <div className="flex items-center justify-center w-full pb-16">
                                 <p className="text-slate-400">
@@ -83,7 +137,6 @@ export default function About({ auth }) {
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         </General>
--- resources/js/Pages/Contact.jsx
+++ resources/js/Pages/Contact.jsx
@@ -11,16 +11,15 @@ import { useEffect } from "react";
 import "aos/dist/aos.css";
 
 export default function Contact({ auth }) {
-
     const submit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
 
-        console.log('submit')
-    }
+        console.log("submit");
+    };
 
     const onHandleChange = (e) => {
-        console.log('')
-    }
+        console.log("");
+    };
 
     useEffect(() => {
         AOS.init();
@@ -29,50 +28,63 @@ export default function Contact({ auth }) {
     return (
         <General auth={auth}>
             <Head title="Contact" />
-             <div className="flex flex-col items-start pt-16 min-h-[calc(100vh-125px)]">
-                <div className="flex mx-auto w-full" data-aos="fade-right">
-                    <h2 className="mx-auto text-medium pt-10 pb-10 text-4xl text-gray-700 font-semibold">Any <span className="text-indigo-800">questions</span> ? Contact <span className="text-indigo-800">us</span> !</h2>
+            <div className="py-16 lg:py-28 min-h-[calc(100vh-125px)]">
+                <div data-aos="fade-right">
+                    <h2 className="mx-auto w-[90%] text-center text-medium text-4xl text-gray-700 font-semibold">
+                        Any <span className="text-indigo-800">questions</span> ?
+                        Contact <span className="text-indigo-800">us</span> !
+                    </h2>
                 </div>
-                <div data-aos="fade-left" className="flex lg:flex-row flex-col items-center justify-between mx-auto w-3/4">
-                    <Player
-                        autoplay
-                        loop
-                        src="/assets/94599-contact-us.json"
-                    >
-                    </Player>
-                    <div className=" flex flex-col w-1/2 p-10">
-                        <h3 className="text-3xl mb-10"><span className="text-indigo-800">Our</span> Informations</h3>
+                <div
+                    data-aos="fade-left"
+                    className="flex flex-col lg:flex-row items-center justify-center mr-10 lg:mt-20 gap-10"
+                >
+                    <div className="max-w-lg h-auto 2xl:max-w-3xl 2xl:pr-44">
+                        <Player
+                            autoplay
+                            loop
+                            src="/assets/94599-contact-us.json"
+                            // style={{
+                            //     maxHeight: "500px",
+                            //     maxWidth: "500px",
+                            //     marginRight: "100px",
+                            // }}
+                        ></Player>
+                    </div>
+                    <div className="flex flex-col 2xl:pr-16">
+                        <h3 className="text-3xl mb-10">
+                            <span className="text-indigo-800">Our</span>{" "}
+                            Informations
+                        </h3>
                         <div className="flex flex-row items-center mb-10">
                             <AiOutlinePhone
                                 size={30}
                                 className="mr-3 text-indigo-800"
                             />
-                            <p 
-                                className="font-medium hover:underline cursor-pointer"
-                            >
+                            <p className="font-medium hover:underline cursor-pointer">
                                 +32 00 00 00
                             </p>
                         </div>
                         <div className="flex flex-row items-center mb-10">
-                            <IoMail 
-                                size={30} 
+                            <IoMail
+                                size={30}
                                 className="mr-3 text-indigo-800"
                             />
-                            <p 
-                                className="font-medium hover:underline cursor-pointer"
-                            >
+                            <p className="font-medium hover:underline cursor-pointer">
                                 support@school-helper.com
                             </p>
                         </div>
                         <div className="flex flex-row items-center">
-                            <GrCircleInformation 
+                            <GrCircleInformation
                                 size={30}
                                 className="mr-3 text-indigo-800"
                             />
-                            <p 
-                                className="font-medium hover:underline cursor-pointer"
-                            >
-                                The <span className="text-indigo-800">team</span> is avaible to resolve <span className="text-indigo-800">your</span> issues.
+                            <p className="font-medium ">
+                                The{" "}
+                                <span className="text-indigo-800">team</span> is
+                                avaible to resolve{" "}
+                                <span className="text-indigo-800">your</span>{" "}
+                                issues.
                             </p>
                         </div>
                     </div>
