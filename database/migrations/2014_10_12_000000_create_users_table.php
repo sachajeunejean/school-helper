3cd93c002781fc357975a88a44f1ac7b910947d5
sachajeunejean <sacha.jeunejean@gmail.com>
2023-02-09 09:16:34 +0100

    updated session variable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,10 @@ return new class extends Migration
      */
     public function up()
     {
+
         DB::statement('SET SESSION sql_require_primary_key=0');
 
+        
         Schema::create('users', function (Blueprint $table) {
             $table->id()->primary();
             $table->string('username')->unique();
