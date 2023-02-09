3492c091fc69ed83ef3a9945ca7af0165176d111
sachajeunejean <sacha.jeunejean@gmail.com>
2023-02-09 14:32:39 +0100

    vite.config changes

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react';
 
 import requireTransform from 'vite-plugin-require-transform'
 
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
+
 export default defineConfig({
     plugins: [
+        viteCommonjs(),
         requireTransform({}),
         laravel({
             input: 'resources/js/app.jsx',
