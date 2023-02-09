523c13f38891ac4283a6e9958b305fdd97ae8fae
sachajeunejean <sacha.jeunejean@gmail.com>
2023-02-09 10:51:58 +0100

    require-transform plugin added

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
+import requireTransform from 'vite-plugin-require-transform'
+
 export default defineConfig({
     plugins: [
+        requireTransform({}),
         laravel({
             input: 'resources/js/app.jsx',
             refresh: true,
