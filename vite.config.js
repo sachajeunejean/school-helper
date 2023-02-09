a7a449b3d519bdc6af77f670d3baf455de14a113
sachajeunejean <sacha.jeunejean@gmail.com>
2023-02-09 18:49:54 +0100

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import commonjs from "@rollup/plugin-commonjs";
 
 export default defineConfig({
     plugins: [
-        commonjs({transformMixedEsModules:true}),
         laravel({
             input: 'resources/js/app.jsx',
             refresh: true,
