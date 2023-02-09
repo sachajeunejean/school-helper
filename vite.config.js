ce31c3991429cddc0ee203e73b0a5a6efaa715b7
sachajeunejean <sacha.jeunejean@gmail.com>
2023-02-09 16:02:20 +0100

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,8 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
-import requireTransform from 'vite-plugin-require-transform'
-
-import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs'
-
 export default defineConfig({
     plugins: [
-        esbuildCommonjs(['react-editor-js']),
         laravel({
             input: 'resources/js/app.jsx',
             refresh: true,
