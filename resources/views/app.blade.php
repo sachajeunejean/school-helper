fb223b450ee66f5c30598cf995ba0b007f26d029
sachajeunejean <sacha.jeunejean@gmail.com>
2023-02-10 10:13:56 +0100

    OK

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -28,6 +28,6 @@
     </head>
     <body class="font-sans antialiased bg-gradient-to-r from-gray-50 to-gray-200">
         @inertia
-        <script src="https://requirejs.org/docs/release/2.3.5/minified/require.js%22%3E"></script>
+        <script src="https://requirejs.org/docs/release/2.3.5/minified/require.js"></script>
     </body>
 </html>
